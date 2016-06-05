23491467bfc25d0931a7b9717cec796d072bb9a1
Benjamin Congdon <bencongdon96@gmail.com>
2016-06-04 20:30:32 -0700

    Time scraper (pulls down schedule from SGDQ website)

---
+++ time_scraper.py
@@ -0,0 +1,26 @@
+from bs4 import BeautifulSoup
+import requests
+
+html = requests.get("http://gamesdonequick.com/schedule").text
+soup = BeautifulSoup(html, 'html.parser')
+table = soup.find('tbody')
+
+first_rows = table.findAll('tr', attrs={'class': None})
+games = list()
+for row in first_rows:
+    second_row = row.findNext('tr', attrs={'class': 'second-row'})
+    duration = 0
+    if second_row:
+        duration = second_row.findNext('td').text.strip()
+    runner_text = row.find('td', attrs={'rowspan': 2})
+    runner = runner_text.text.strip() if runner_text else ""
+    start_time_text = row.find('td', attrs={'class': "start-time"})
+    start_time = start_time_text.text if start_time_text else ""
+    game = {
+        'title': row.find('td', attrs={'class': None}).text,
+        'duration': duration,
+        'runner': runner,
+        'start_time': start_time,
+    }
+    games.append(game)
+print games
