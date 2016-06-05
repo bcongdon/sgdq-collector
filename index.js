348ea19ac6063d9d813971668a1bf5f072c73cc1
Benjamin Congdon <bencongdon96@gmail.com>
2016-06-04 17:24:05 -0700

    First dump of collector

---
+++ index.js
@@ -0,0 +1,76 @@
+"use strict"
+var irc = require('tmi.js');
+const scrapeIt = require("scrape-it");
+var scheduler = require('node-schedule');
+var firebase = require('firebase');
+
+firebase.initializeApp({
+  serviceAccount: "credentials.json",
+  databaseURL: "https://sgdq-backend.firebaseio.com"
+});
+
+var exports = module.exports;
+
+var client = irc.client();
+var CHANNEL = "GosuGamers"
+exports.DONATION_URL = "https://gamesdonequick.com/tracker/index/sgdq2014"
+
+exports.getTwitchViewers = function(cb) {
+    client.api({
+        url: "https://api.twitch.tv/kraken/streams/" + CHANNEL
+    }, function(err, res, body) {
+        body = JSON.parse(body);
+        if(body && 'stream' in body && "viewers" in body['stream']){
+            cb(body['stream']['viewers'])
+        }
+    });
+}
+
+exports.getCurrentDonations = function(cb) {
+    scrapeIt(exports.DONATION_URL, {
+        donation_string: "small"
+    }).then(function(results) {
+        var res_arr   = results['donation_string'].split('\n');
+        var total_str = res_arr[1];
+        var avg_str   = res_arr[3];
+
+        var total_donations = total_str.split('(')[0].substring(1).replace(",","");
+        var num_donators    = /\(.+\)/.exec(total_str)[0].slice(1, -1);
+
+        var max_donation = avg_str.split('/')[0].substring(1).replace(",","");
+        var avg_donation = avg_str.split('/')[1].substring(1).replace(",","");
+
+        cb({
+            total:    parseFloat(total_donations),
+            donators: parseInt(num_donators), 
+            max:      parseFloat(max_donation),
+            avg:      parseFloat(avg_donation)
+        });
+    });
+}
+
+var db = firebase.database();
+var data = db.ref("/data");
+var stats = db.ref("/stats")
+
+var time;
+
+// Run every 1 minute
+var currSeconds = new Date().getSeconds();
+scheduler.scheduleJob(currSeconds + " * * * * *", function(){
+    time = new Date();
+    console.log("Running scheduled check at " + time.toString("yyyy-MM-dd HH:mm:ss"));
+    exports.getTwitchViewers(function(viewers){
+        data.child(time.getTime()).child('v').set(viewers);
+    });
+    exports.getCurrentDonations(function(obj){
+        data.child(time.getTime()).child('m').set(obj.total);
+        data.child(time.getTime()).child('d').set(obj.donators);
+
+        stats.child("total_donations").set(obj.total);
+        stats.child("num_donators").set(obj.donators);
+        stats.child("max_donation").set(obj.max);
+        stats.child("avg_donation").set(obj.avg);
+
+    });
+});
---
+++ test.js
@@ -0,0 +1,20 @@
+var assert = require('assert');
+var expect = require('chai').expect;
+
+var collector = require('./index.js');
+
+describe('index.js', function(){
+    describe("getCurrentDonations()", function() {
+        it('should get 2014 data correctly', function(done){
+            collector.DONATION_URL = "https://gamesdonequick.com/tracker/index/sgdq2014"
+            collector.getCurrentDonations(function(res){
+                console.log(res)
+                assert.equal(res.total, 718235.07)
+                assert.equal(res.donators, 19104)
+                assert.equal(res.max, 82984.71)
+                assert.equal(res.avg, 37.6)
+                done();
+            });
+        });
+    });
+});
