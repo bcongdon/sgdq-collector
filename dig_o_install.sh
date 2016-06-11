3d88df55965dbac5bdfe0524561edf7b89eae215
Benjamin Congdon <bencongdon96@gmail.com>
2016-06-11 16:25:40 -0700

    Digital Ocean install script, renamed index to something more descriptive

--- dig_o_install.sh
+++ dig_o_install.sh
@@ -1,5 +1,7 @@
 # Designed to be run on a DigitalOcean Node.js droplet
-apt-get install git -y
+apt-get install git mongodb-clients mongodb-server -y
+mkdir -p /data/db
+mongod --fork --logpath /var/log/mongod.log
 git clone https://github.com/bcongdon/sgdq-collector
 cd sgdq-collector
 npm install
