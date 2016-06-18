62e3d83756409f37ccf3ef7f5eb1fd52987a7211
Benjamin Congdon <bencongdon96@gmail.com>
2016-06-18 12:20:10 -0700

    added main to channel.js

--- utils/channel.js
+++ utils/channel.js
@@ -3,19 +3,26 @@ var client = irc.client();
 
 var exports = module.exports;
 
-var channel = "ESL_DOTA2";
+var channel = "NALCS1";
 
 exports.channel = function(){
   getChannel();
   return channel;
 }
 
-function getChannel() {
+function getChannel(cb) {
   client.api({
       url: "https://api.twitch.tv/kraken/streams/featured",
   }, function(err, res, body) {
       channel = JSON.parse(body)['featured'][0]['stream']['channel']['display_name'];
+      if(cb) cb(channel);
   });
 }
 
 getChannel();
+
+if (require.main === module) {
+  getChannel(function(e){
+    console.log(e);
+  });
+}
