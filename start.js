f920433d4002c720f8a3afca306be29fc918a027
Benjamin Congdon <bencongdon96@gmail.com>
2016-06-20 19:47:16 -0700

    Forever logging

--- start.js
+++ start.js
@@ -1,8 +1,17 @@
 var forever = require('forever')
 
-var sgdq_collector  = new (forever.Monitor)('sgdq_collector.js');
-var chat_collector  = new (forever.Monitor)('chat_collector.js');
-var tweet_collector = new (forever.Monitor)('tweet_collector.js');
+var sgdq_collector  = new (forever.Monitor)('sgdq_collector.js',{
+  'outFile': './sgdq-collector.log',
+  'errFile': './sgdq-err.log'
+});
+var chat_collector  = new (forever.Monitor)('chat_collector.js',{
+  'outFile': './chat-collector.log',
+  'errFile': './chat-err.log'
+});
+var tweet_collector = new (forever.Monitor)('tweet_collector.js',{
+  'outFile': './tweet-collector.log',
+  'errFile': './tweet-err.log'
+});
 
 sgdq_collector.start();
 chat_collector.start();
