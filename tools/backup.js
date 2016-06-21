42f8870b12f4884b35632aa63b82a58097bbf082
Benjamin Congdon <bencongdon96@gmail.com>
2016-06-20 21:06:07 -0700

    Backup deletion after ~60 minutes

--- tools/backup.js
+++ tools/backup.js
@@ -8,7 +8,8 @@ var mkdirp = require('mkdirp');
 var expandHomeDir = require('expand-home-dir');
 var ref = firebase.database().ref();
 var fs = require('fs');
-var dateutils = require('../utils/time_utils.js')
+var dateutils = require('../utils/time_utils.js');
+var path = require('path');
 
 var backup_directory = "~/sgdq-backup/"
 
@@ -23,8 +24,18 @@ function doBackup(cb) {
     var timestamp = dateutils.getTimeStamp();
     fs.writeFile(backup_directory + (new Date(timestamp)).toString() + '.json', JSON.stringify(data.val()), function(err) {
       if(err) console.log(err);
+      // Delete old logs
+      fs.readdir(backup_directory, function(err, files){
+        files.sort(function(a, b) {
+               return fs.statSync(backup_directory + b).mtime.getTime() - 
+                      fs.statSync(backup_directory + a).mtime.getTime();
+        });
+        // Delete all but the 60 most recent files
+        files = files.filter(function(a) { return path.extname(a) == '.json'}).slice(60);
+        files.forEach(function(d) { fs.unlink(backup_directory + d); console.log("Deleting: " + d) });
+      });
     });
-    cb(data.val());
+    if(cb) cb(data.val());
   });
 }
 
