460ea482c890905178c56de8b59cb61389d22ed8
Benjamin Congdon <bencongdon96@gmail.com>
2016-06-26 06:09:15 -0700

    Split backup into backup + health_check

--- backup.js
+++ backup.js
@@ -10,11 +10,7 @@ var ref = firebase.database().ref();
 var fs = require('fs');
 var dateutils = require('./utils/time_utils.js');
 var path = require('path');
-var twilioCreds = require('./twilio_creds.json');
-var twilio = require('twilio');
-var client = new twilio.RestClient(twilioCreds.accountSid, twilioCreds.authToken);
-var includes = require('array-includes');
-
+var health_check = require('./health_check.js')
 
 var backup_directory = "~/sgdq-backup/"
 
@@ -44,56 +40,11 @@ function doBackup(cb) {
   });
 }
 
-function healthCheck(data_in){
-  var data = [],
-      extras = [],
-      zeros = {'donators': 0,
-               'donations': 0,
-               'viewers': 0,
-               'tweets': 0,
-               'emotes': 0,
-               'chats': 0};
-  for(var key in data_in.data)   data.push(data_in.data[key]);
-  for(var key in data_in.extras) extras.push(data_in.extras[key]);
-  data = data.reverse();
-  extras = extras.reverse()
-  for(var i = 0; i < 5 && i < data.length && i < extras.length; i++){
-    if(data[i].d <= 0) zeros['donators'] += 1;
-    if(data[i].m <= 0) zeros['donations'] += 1;
-    if(data[i].v <= 0) zeros['viewers'] += 1;
-    if(extras[i].t <= 0) zeros['tweets'] += 1;
-    if(extras[i].e <= 0) zeros['emotes'] += 1;
-    if(extras[i].c <= 0) zeros['chats'] += 1;
-  }
-  var alarms = []
-  for(var key in zeros) if(zeros[key] >= 3) alarms.push([key, "No data from *" + key + "* in " + zeros[key] + " minutes!"]);
-  if(alarms.length > 0) sendAlarms(alarms);
-}
 
-var triggered = []
-
-function sendAlarms(alarms) {
-  alarms = alarms.filter(function(d) { return !includes(triggered, d[0]) });
-  var message = alarms.map(function(d) { return d[1]; }).join("\n");
-  client.messages.create({
-      body: message,
-      to: twilioCreds.alertNumber,
-      from: twilioCreds.twilioNumber
-  }, function(err, message) {
-      if(err) {
-        console.log(err);
-        return;
-      }
-      alarms.forEach(function(d) { 
-        console.log("Alarm triggered on: " + d[0]);
-        triggered.push(d[0]);
-      });
-  });
-}
 
 // console.log("*Backup started.")
 // schedule.scheduleJob({second: (new Date()).getSeconds()}, function(){
   var timestamp = dateutils.getTimeStamp();
   console.log((new Date(timestamp)).toString() + " - Creating backup");
-  doBackup(healthCheck);
+  doBackup(health_check.check);
 // }
---
+++ health_check.js
@@ -0,0 +1,55 @@
+var twilioCreds = require('./twilio_creds.json');
+var twilio = require('twilio');
+var client = new twilio.RestClient(twilioCreds.accountSid, twilioCreds.authToken);
+var includes = require('array-includes');
+
+
+var exports = module.exports;
+
+exports.check = function(data_in){
+  console.log("hello")
+  var data = [],
+      extras = [],
+      zeros = {'donators': 0,
+               'donations': 0,
+               'viewers': 0,
+               'tweets': 0,
+               'emotes': 0,
+               'chats': 0};
+  for(var key in data_in.data)   data.push(data_in.data[key]);
+  for(var key in data_in.extras) extras.push(data_in.extras[key]);
+  data = data.reverse();
+  extras = extras.reverse()
+  for(var i = 0; i < 5 && i < data.length && i < extras.length; i++){
+    if(data[i].d <= 0) zeros['donators'] += 1;
+    if(data[i].m <= 0) zeros['donations'] += 1;
+    if(data[i].v <= 0) zeros['viewers'] += 1;
+    if(extras[i].t <= 0) zeros['tweets'] += 1;
+    if(extras[i].e <= 0) zeros['emotes'] += 1;
+    if(extras[i].c <= 0) zeros['chats'] += 1;
+  }
+  var alarms = []
+  for(var key in zeros) if(zeros[key] >= 3) alarms.push([key, "No data from *" + key + "* in " + zeros[key] + " minutes!"]);
+  if(alarms.length > 0) sendAlarms(alarms);
+}
+
+var triggered = []
+
+function sendAlarms(alarms) {
+  alarms = alarms.filter(function(d) { return !includes(triggered, d[0]) });
+  var message = alarms.map(function(d) { return d[1]; }).join("\n");
+  client.messages.create({
+      body: message,
+      to: twilioCreds.alertNumber,
+      from: twilioCreds.twilioNumber
+  }, function(err, message) {
+      if(err) {
+        console.log(err);
+        return;
+      }
+      alarms.forEach(function(d) { 
+        console.log("Alarm triggered on: " + d[0]);
+        triggered.push(d[0]);
+      });
+  });
+}
