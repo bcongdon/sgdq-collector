6a3edcd766bfc69b173fa623bea5c26d3a4dff66
Benjamin Congdon <bencongdon96@gmail.com>
2016-06-30 06:49:08 -0700

    Updated health check, misc formatting

--- misc/google_install.sh
+++ misc/google_install.sh
@@ -6,9 +6,5 @@ n lts
 git clone https://github.com/bcongdon/sgdq-collector
 cd sgdq-collector
 npm install
-cd ..
 npm install -g forever
-echo "forever start sgdq-collector/index.js" > start.sh
-chmod +x start.sh
-vim sgdq-collector/credentials.json
-
+tmux
