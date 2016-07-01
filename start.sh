6cdd4291e93bfc5f378d4cee07fa95ec8eecca6e
Benjamin Congdon <bencongdon96@gmail.com>
2016-07-01 07:50:39 -0700

    Update start.sh to do logs

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-forever start sgdq_collector.js
-forever start chat_collector.js
-forever start tweet_collector.js
-forever start storage_link.js
+forever start -o ./sgdq_collector.log  -e ./sgdq_collector.err  -f sgdq_collector.js
+forever start -o ./chat_collector.log  -e ./chat_collector.err  -f chat_collector.js
+forever start -o ./tweet_collector.log -e ./tweet_collector.err -f tweet_collector.js
+forever start -o ./storage_link.log    -e ./storage_link.err    -f storage_link.js
